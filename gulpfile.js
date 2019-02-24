e5c9dee7dc01d542286bcce81cd74337b6a7bee6
Stephen Leachman <36422934+sjleachman@users.noreply.github.com>
2019-02-24 09:06:50 -0500

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const gulp         = require('gulp');
+const browserSync  = require('browser-sync').create();
+const sass         = require('gulp-sass');
+
+// Compile Sass & Inject Into Browser
+gulp.task('sass', function() {
+  return gulp.src(['node_modules/materialize-css/sass/materialize.scss', 'public/sass/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest("public/css"))
+    .pipe(browserSync.stream());
+});
+
+// Move JS Files to src/js
+gulp.task('js', function(){
+  return gulp.src(['node_modules/materialize-css/dist/js/materialize.min.js'])
+    .pipe(gulp.dest("public/js"))
+    .pipe(browserSync.stream());
+});
+
+// Watch Sass & Serve
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./public"  
+    });
+
+    gulp.watch(['node_modules/materialize-css/sass/materialize.scss','public/scss/*.scss'], ['sass']);
+    gulp.watch("public/*.html").on('change', browserSync.reload);
+});
+
+// Move Fonts Folder to src/fonts
+gulp.task('fonts', function(){
+  return gulp.src('node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest("public/fonts"));
+});
+
+// Move Font Awesome CSS to src/css
+gulp.task('fa', function(){
+  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+    .pipe(gulp.dest("public/css"));
+});
+
+
+// Default Task
+gulp.task('default', ['js', 'serve', 'fonts', 'fa']);
